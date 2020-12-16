907727e9b2e741f68da2c0934d8e4a4923f2c767
Arun George Zachariah <azachariah@mail.missouri.edu>
2020-12-16 13:00:04 -0600

    Adding load test.

--- src/edu/missouri/Exec.java
+++ src/edu/missouri/Exec.java
@@ -65,8 +65,45 @@ public class Exec {
         DropTable.getInstance().deleteTable(tableName);
     }
 
+    private static void loadTest(long n) {
+        String tableName = "TestTable";
+        System.out.println("Creating table :: " + tableName);
+
+        long id = CreateTable.getInstance().createTable(tableName);
+        System.out.println("Successfully created table :: id :: " + id);
+
+        String value = null;
+        try(BufferedReader br = new BufferedReader(new FileReader(new File("data/data.txt")))) {
+            value = br.readLine();
+            System.out.println("Size of value :: " + value.getBytes("UTF-8").length);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Long startTime = System.currentTimeMillis();
+        for(long i=0; i<n; i++) {
+            String key = String.valueOf(i);
+
+            System.out.println("Adding to the table :: key :: " + key);
+            InsertToTable.getInstance().writeToRAMCloud(id, key, value);
+        }
+        Long writeEndTime = System.currentTimeMillis();
+        System.out.println("Time taken to insert :: n :: " + n + " :: entries is :: " + (writeEndTime - startTime) + " ms");
+
+        System.out.println("Retrieving value for :: key :: 0");
+        ReadFromTable.getInstance().readFromRAMCloud(id, "0");
+        Long readEndTime = System.currentTimeMillis();
+
+        System.out.println("Time taken to read key 0 is :: " + (readEndTime - startTime) + " ms");
+
+        System.out.println("Dropping the table :: id :: " + id);
+        DropTable.getInstance().deleteTable(tableName);
+
+    }
+
     public static void main(String[] args) {
         basicTest();
         keySizeTest();
+        loadTest(10);
     }
 }
