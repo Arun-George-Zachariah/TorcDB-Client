2ffcddae36bdec7a1408262b62add9165854c072
Arun George Zachariah <azachariah@mail.missouri.edu>
2021-01-09 14:04:34 -0600

    Changes done for DPDK.

--- src/edu/missouri/Constants/Constants.java
+++ src/edu/missouri/Constants/Constants.java
@@ -3,5 +3,8 @@ package edu.missouri.Constants;
 public class Constants {
     public static final String RC_COORDINATOR_LOC = "ramcloudCoordinatorLocator";
     public static final String SERVER_SPAN = "serverSpan";
+    public static final String DPDK_PORT = "dpdkPort";
+
     public static final String RAMCLOUD_LIB = "ramcloud_java";
+    public static final String RC_CLUSTER_NAME = "main";
 }
--- src/edu/missouri/Operations/RAMCloudInstance.java
+++ src/edu/missouri/Operations/RAMCloudInstance.java
@@ -6,16 +6,11 @@ import edu.stanford.ramcloud.Util;
 
 public class RAMCloudInstance {
     public static RAMCloudInstance instance = null;
-    public static String coordinatorLocator = null;
     public static RAMCloud client = null;
 
     public static RAMCloudInstance getInstance() {
         if(instance == null) {
             Util.loadLibrary("ramcloud_java");
-
-            coordinatorLocator = System.getProperty(Constants.RC_COORDINATOR_LOC);
-            System.out.println("RAMCloudInstance :: getInstance :: coordinatorLocator :: " + coordinatorLocator);
-
             instance = new RAMCloudInstance();
         }
         return instance;
@@ -24,7 +19,18 @@ public class RAMCloudInstance {
     public static RAMCloud getClient() {
         if(client == null) {
             System.out.println("RAMCloudInstance :: getInstance :: Creating RAMCloud client with coordinatorLocator :: " + coordinatorLocator);
-            client = new RAMCloud(coordinatorLocator);
+
+            String coordinatorLocator = System.getProperty(Constants.RC_COORDINATOR_LOC);
+            System.out.println("RAMCloudInstance :: getInstance :: coordinatorLocator :: " + coordinatorLocator);
+
+            int dpdkPort = -1;
+            if(System.getProperty(Constants.DPDK_PORT) != null) {
+                dpdkPort = Integer.parseInt(System.getProperty(Constants.DPDK_PORT));
+            }
+            System.out.println("RAMCloudInstance :: getInstance :: dpdkPort :: " + dpdkPort);
+
+            client = new RAMCloud(coordinatorLocator, Constants.RC_CLUSTER_NAME, dpdkPort);
+
         }
         return client;
     }
